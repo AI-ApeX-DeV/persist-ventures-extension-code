40ad32eae56c75491057bc9fe0209d31665ac6a5
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-07-25 16:50:28 +0530

    done

---
+++ background.js
@@ -0,0 +1,47 @@
+chrome.action.onClicked.addListener(function (tab) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: fillForm
+    });
+  });
+  
+  function fillForm() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'fill_form' });
+    });
+  }
+
+  // background.js
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/background.js') // Replace with the correct path to your background.js file
+      .then(registration => {
+        console.log('Service worker registered!', registration);
+      })
+      .catch(error => {
+        console.error('Service worker registration failed:', error);
+      });
+  }
+  
+  // Add event listeners to keep the service worker active
+  self.addEventListener('install', event => {
+    event.waitUntil(self.skipWaiting());
+  });
+  
+  self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+  });
+  
+  // Add event listeners to respond to fetch and message events
+  self.addEventListener('fetch', event => {
+    // Implement your fetch event handling logic here
+  });
+  
+  self.addEventListener('message', event => {
+    // Implement your message event handling logic here
+  });
+  
+  
+  
---
+++ content.js
@@ -0,0 +1,22 @@
+chrome.runtime.onMessage.addListener(function (message,sender,sendResponse){
+    if(message.action ==="fill_form"){
+        const openAiApiKey="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";
+        const formData=getFormDataFromSomewhere();
+        let url = "https://api.openai.com/v1/engines/davinci-codex/completions";
+        const prompt="fill in the form with the following information:"+JSON.stringify(formData);
+        axios.post(url,{
+            prompt:prompt,
+            max_tokens:100,
+            temperature:0.7
+        },{
+            headers:{
+                'Authorization': `Bearer ${openAiApiKey}`,
+            }
+        }).then(response=>{
+            const filledForm=response.data.choices[0].text;
+        }).catch(error=>{
+            console.error("error while calling OpenAi API:",error);
+        });
+        
+    }
+});
